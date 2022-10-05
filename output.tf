05a09c9a7678859a49dde969475f08caa27bab57
alex <alexey.v.gaydukov@yandex.ru>
2022-10-05 17:35:53 +0300

    fixed output for multiple vms

--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
 output "ip_address_1" {
-        value = values(yandex_compute_instance.vm-1)[*].network_interface.0.ip_address
+        value = yandex_compute_instance.vm-1.*.network_interface.0.ip_address
     }
